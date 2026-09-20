2c437373d80828de645b59c143220710023ef805
M.S Édit <mairy.gillen@gmail.com>
2026-09-20 19:31:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2353,8 +2353,15 @@ def artisan():
         FROM demandes
         JOIN particuliers
             ON demandes.particulier_id = particuliers.id
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM artisan_demandes_supprimees
+            WHERE artisan_demandes_supprimees.artisan_id = ?
+            AND artisan_demandes_supprimees.demande_id = demandes.id
+        )
         ORDER BY demandes.id DESC
-        """
+        """,
+        (artisan_user["id"],)
     ).fetchall()
 
     conn.close()
